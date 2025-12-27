0603a92a3f104e89d331d6eb94b0bbc9a6843de9
obrucheoghene <obrucheoghene1@gmail.com>
2025-12-27 09:12:11 +0100

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RecordingModule } from './recording/recording.module';
+import { HealthModule } from './health/health.module';
+import { GlobalExceptionFilter } from './common/filters/exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    RecordingModule,
+    HealthModule,
+  ],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
